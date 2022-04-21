31a93079d469374d2fbfadfa19294d969f5b435e
jimmydinh211 <minhdinhtranhoang@gmail.com>
2022-04-21 15:31:20 -0400

    Update

--- wiki/jimmipedia/urls.py
+++ wiki/jimmipedia/urls.py
@@ -1,7 +1,10 @@
+from turtle import home
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("index", views.index)
+    path("index", views.home),
+    path("random_info_page", views.random_method),
+    path("<str:name>", views.index)
 ]
--- wiki/jimmipedia/views.py
+++ wiki/jimmipedia/views.py
@@ -1,6 +1,22 @@
+from multiprocessing import context
+from typing import List
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import util
+import random
 
 # Create your views here.
-def index(request):
+def index(request, name):
+    if (util.get_entry(name) == None):
+        return render(request, "jimmipedia/error.html")
+    else:
+        return render(request, "jimmipedia/info_page.html", {"view_name": util.get_entry(name)})
+
+#Homepage
+def home (request):
     return render(request, "jimmipedia/index.html")
+
+def random_method(request):
+    ent = util.list_entries()
+    name = random.choice(ent)
+    return render(request, "jimmipedia/random_info_page.html", {"view_name": util.get_entry(name)})
